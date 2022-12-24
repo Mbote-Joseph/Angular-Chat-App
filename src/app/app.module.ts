76a62f5ad5efd80555b0bc2e0dc77584119d2936
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-12-24 06:58:14 +0300

    Angular Chat App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ChatareaComponent } from './chatarea/chatarea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    ChatareaComponent
   ],
   imports: [
     BrowserModule,
